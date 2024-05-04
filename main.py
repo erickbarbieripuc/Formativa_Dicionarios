fd4b545bc91f4cd7a06e04cb9dac77930b676556
erickbarbieripuc <167023009+erickbarbieripuc@users.noreply.github.com>
2024-05-03 22:20:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# #Atividade 1
+# dicionario = {}
+# 
+# quantidade = int(input("Digite a quantidade de personagens e suas variaveis:"))
+#
+# for i in range(quantidade):
+#     chave = input("Digite o personagem:")
+#     valor = input("Digite a sua variavel:")
+#     dicionario[chave] = valor
+#
+# print("personagens e suas respectivas variaveis:", dicionario)
